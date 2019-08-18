4330677a8f906dcb2fd598200c6e8cfedcb718d1
brianchangtw <paul.jonez.tw@gmail.com>
2019-08-18 23:31:21 +0800

    Java and Kotlin variable, data type

--- src/main/java/com/brian/Hello.java
+++ src/main/java/com/brian/Hello.java
@@ -6,6 +6,17 @@ public class Hello {
 //        new Person().hello();
         Person p = new Person();
         p.hello();
+        int age = 19;
+        char c = 'A';
+        byte b = 120;
+        float weight = 66.5f;
+        float height = 1.7f;
+        boolean adult = true;
+        boolean enroll = false;
+        // Reference data type
+        String name = "Brian";
+        Integer age2 = 19;
+        Character c2 = 'A';
     }
 
 }
